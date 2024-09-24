96cc732ffb874db667802b13e7582af085865379
Nour Bayar <nourbayar4@gmail.com>
2024-09-24 20:38:43 +0100

    MonProjet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './binding/binding.component';
+import { MonProjetComponent } from './mon-projet/mon-projet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent,
+    MonProjetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mon-projet/mon-projet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-projet',
+  templateUrl: './mon-projet.component.html',
+  styles: ``
+})
+export class MonProjetComponent {
+
+}
